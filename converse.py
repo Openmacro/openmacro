d412df006624920fbd179b3c14c4650893e79534
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-08-10 23:36:21 +0900

    chore: update converse.py
    exponetial -> exponential

--- converse.py
+++ converse.py
@@ -2,4 +2,4 @@ from openmacro import macro
 
 if __name__ == '__main__':
     while True:
-        macro.chat(input("\nUser> ") or "plot an example exponetial graph") #"What is 450 * 69 + 68") # "Is Inside Out 2 playing in hoyts today?")
+        macro.chat(input("\nUser> ") or "plot an example exponential graph") #"What is 450 * 69 + 68") # "Is Inside Out 2 playing in hoyts today?")
